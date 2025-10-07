15ec4775d1ba74c046500b25e2b79e5b91413b54
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-10-07 18:35:11 +0300

    add header

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import UsersListPage from "./pages/users/list";
 import {Routes,Route} from "react-router"
 import UserRegisterPage from "./pages/users/register";
+import UserLayout from "./layouts/userLayout.tsx";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path={"/"}>
+                <Route path={"/"} element={<UserLayout/>}>
                     <Route index element={<UsersListPage/>} />
                     <Route path={"register"} element={<UserRegisterPage/>} />
                 </Route>
---
+++ my-app/src/layouts/userLayout.tsx
@@ -0,0 +1,34 @@
+import {Link, Outlet} from "react-router";
+
+const UserLayout: React.FC = () => {
+    return (
+        <div className="min-h-screen flex flex-col bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
+            <header className="w-full py-4 px-6 bg-yellow-500 text-white shadow-md flex items-center">
+                <div className="ml-auto flex space-x-4">
+                    <Link
+                        to="login"
+                        className="bg-yellow-700 text-white px-4 py-2 rounded hover:bg-orange-100 hover:text-yellow-800 transition"
+                    >
+                        Вхід
+                    </Link>
+
+                    <Link
+                        to="register"
+                        className="bg-yellow-700 text-white px-4 py-2 rounded hover:bg-orange-100 hover:text-yellow-800 transition"
+                    >
+                        Реєстрація
+                    </Link>
+                </div>
+            </header>
+
+            <main className="flex-1 p-4 md:p-6">
+                <Outlet />
+            </main>
+
+            <footer className="w-full py-3 px-6 bg-gray-100 text-sm text-center dark:bg-gray-800 dark:text-gray-300">
+                © 2025 ATB. Усі права захищено.
+            </footer>
+        </div>
+    );
+};
+export default UserLayout;
