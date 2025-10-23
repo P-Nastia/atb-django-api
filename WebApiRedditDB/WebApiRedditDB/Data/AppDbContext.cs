c740a9c96eb03cf2c97256772aa2f0c09d7c630e
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-10-23 19:13:30 +0300

    add table topic

---
+++ WebApiRedditDB/WebApiRedditDB/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiRedditDB.Data.Entities;
+
+namespace WebApiRedditDB.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<TopicEntity> Topics { get; set; }
+}
--- WebApiRedditDB/WebApiRedditDB/Program.cs
+++ WebApiRedditDB/WebApiRedditDB/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiRedditDB.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DeffaultConnection")));
+
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
